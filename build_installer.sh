04fc5f2ab4414a8c8a811e51402105986e65789d
Rada Berar <rada.berar@ucircuit.com>
2017-07-13 14:21:26 +0200

    Added option to switch to next workspace

--- build_installer.sh
+++ build_installer.sh
@@ -2,13 +2,15 @@
 
 cd "$(dirname "$0")"
 
-cp -fr source/* wswitch-1.1/opt/wswitch/
+mkdir wswitch-1.2/opt/wswitch
+cp -fr source/* wswitch-1.2/opt/wswitch/
 
-chmod -R 755 wswitch-1.1/DEBIAN/
+chmod -R 755 wswitch-1.2/DEBIAN/
 
 rm -f ./*.deb
 
-dpkg-deb --build ./wswitch-1.1
+dpkg-deb --build ./wswitch-1.2
 
-mv wswitch-1.1.deb wswitch_install-1.1.deb
+mv wswitch-1.2.deb wswitch_install-1.2.deb
 
+rm -fr wswitch-1.2/opt/wswitch
